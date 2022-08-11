711e126b7279ef494767f06134131396294d3b04
Di <dimas2343@mail.ru>
2022-08-11 21:05:13 +0300

    SecondGigitNuber

--- 001_163 показать 2ю цифру(6)/Program.cs
+++ 001_163 показать 2ю цифру(6)/Program.cs
@@ -1,26 +1,43 @@
 ﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-
 // 456 -> 5
 // 782 -> 8
 // 918 -> 1
 
+// 1. без методов*********************************************************
+// Console.Clear();
+// Console.WriteLine("Введите трехзначное число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int secondDigit = (number / 10) % 10;
+
+// if (number > 100 & number < 1000)
+// {
+//     Console.WriteLine(secondDigit);          
+// }
+// else
+// {
+//     Console.WriteLine("Не верный ввод");
+// }
+
+// 2. c методом***********************************************************
 Console.Clear();
-
-Console.WriteLine("Введите трехзначное число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-
-int secondDigit = (number / 10) % 10;
-
-//Console.WriteLine(secondDigit);
-// с проверкой
-
+int number = ReadInt("Введите трехзначное число: ");
 if (number > 100 & number < 1000)
 {
-    Console.WriteLine(secondDigit);
+    Console.WriteLine(SecDigitNum(number));         
 }
 else
 {
     Console.WriteLine("Не верный ввод");
 }
+// ***********************************************************************
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
 
-// метод не писал, т.к. их дольше писать, чем они сокращали бы время, для таких задач
+int SecDigitNum (int number)
+{
+    int result = (number / 10) % 10;
+    return result;
+}
---
+++ 002_23456 вывести тритью цифру(4)/Program.cs
@@ -0,0 +1,58 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+// 1. Без методов
+// Console.Clear();
+// Console.WriteLine("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int n = number;
+
+// while (n > 999)
+// {
+//     n = n / 10;    
+// }
+// if (n < 1000 & n > 99)
+// {
+//     n = n % 10;
+//     Console.WriteLine(n);
+// }
+// else
+// {
+//    Console.WriteLine("третьей цифры нет"); 
+// }
+
+
+// 2. C методом******************
+// **************************************************************
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int threeDigit(int number)
+{
+    int result = number %10;
+    return result;
+}
+// **************************************************************
+Console.Clear();
+int number = ReadInt("Введите трехзначное число: ");
+int n = number;
+
+while (number > 999)
+{
+    number = number / 10;
+}
+if (number < 1000 & number > 99)
+{
+    Console.WriteLine(threeDigit(number));
+}
+else
+{
+   Console.WriteLine("третьей цифры нет"); 
+}
+
+
